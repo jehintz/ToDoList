9506757b5b08cff5ce8e10f5f67ab7c7465f234d
Jessica Hintz <jehintz@gmail.com>
2015-03-26 22:24:08 -0700

    Implemented existing methods in the TaskRepository and made a constructor for the same

--- iTrellisToDoList/TaskRepository.cs
+++ iTrellisToDoList/TaskRepository.cs
@@ -9,26 +9,61 @@ namespace iTrellisToDoList
     {
         private List<Task> tasksList;
 
+        //Constructor
+        public TaskRepository()
+        {
+            tasksList = new List<Task>();
+        }
+
+        //Add a task with NO due date
         private void AddTask(string title, string description)
         {
-            throw new NotImplementedException();
+            Task newTask = new Task(title, description);
+            tasksList.Add(newTask);
         }
 
+        //Add a task WITH a due date
         private void AddTask(string title, string description, DateTime dueDate)
         {
-            throw new NotImplementedException();
+            Task newTask = new Task(title, description, dueDate);
+            tasksList.Add(newTask);
         }
 
+        //Remove a specific task from the list
         private void RemoveTask(Task taskToRemove)
         {
-            throw new NotImplementedException();
+            if (tasksList.Count > 0)
+            {
+                for (int i = 0; i < tasksList.Count; i++)
+                {
+                    if (tasksList[i] == taskToRemove)
+                    {
+                        tasksList.RemoveAt(i);
+                        break;
+                    }
+                }
+            }
+            throw new ArgumentException("Task not found.");
         }
 
+        //Mark a specific task as completed
         private void CompleteTask(Task taskToComplete)
         {
-            throw new NotImplementedException();
+            if (tasksList.Count > 0)
+            {
+                foreach (var t in tasksList)
+                {
+                    if (t == taskToComplete)
+                    {
+                        t.IsCompleted = true;
+                        break;
+                    }
+                }
+            }
+            throw new ArgumentException("Task not found.");
         }
 
+        //Get a list of all tasks
         private List<Task> GetTasks()
         {
             return tasksList;
