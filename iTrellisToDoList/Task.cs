07af70e156155e242add4399e45c748dfc68e45c
Jessica Hintz <jehintz@gmail.com>
2015-03-26 21:38:03 -0700

    Created barebones Task class, which represents 1 to do item, and TaskRepository, which holds a list of tasks and all related methods (not yet implemented)

---
+++ iTrellisToDoList/Task.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace iTrellisToDoList
+{
+    public class Task
+    {
+        //Private fields
+        private string _title;
+        private DateTime _dueDate;
+
+        //Public properties
+        public string Title
+        {
+            get { return _title; }
+            set
+            {
+                if (value == null || value == "")
+                    throw new ArgumentException("Please enter a descriptive title for the task.");
+                _title = value;
+            }
+        }
+        public string Description { get; set; }
+        public DateTime DueDate
+        {
+            get { return _dueDate; }
+            set
+            {
+                if (value < DateTime.Now)
+                    throw new ArgumentException("The due date must be later than the current date.");
+                _dueDate = value;
+            }
+        }
+        public bool IsCompleted { get; set; }
+
+        //Constructors
+        public Task(string title, string details)
+        {
+            Title = title;
+            Description = details;
+            IsCompleted = false;
+        }
+
+        public Task(string title, string details, DateTime dueDate)
+        {
+            Title = title;
+            Description = details;
+            DueDate = dueDate;
+            IsCompleted = false;
+        }
+    }
+}
---
+++ iTrellisToDoList/TaskRepository.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace iTrellisToDoList
+{
+    public class TaskRepository : IEnumerable<Task>
+    {
+        private List<Task> tasksList;
+
+        private void AddTask(string title, string description)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void AddTask(string title, string description, DateTime dueDate)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void RemoveTask(Task taskToRemove)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void CompleteTask(Task taskToComplete)
+        {
+            throw new NotImplementedException();
+        }
+
+        //Implementation of IEnumerable<Task> interface
+        public IEnumerator<Task> GetEnumerator()
+        {
+            return tasksList.GetEnumerator();
+        }
+
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+        {
+            return tasksList.GetEnumerator();
+        }
+    }
+}
