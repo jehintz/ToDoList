0ad209296c26be931dc303798cc3fb080447be7d
Jessica Hintz <jehintz@gmail.com>
2015-03-29 02:55:20 -0700

    Added a LoadSampleTask() method in TaskTests to create a single Task (instead of making it from scratch each time)

--- iTrellisToDoList.Tests/TaskTests.cs
+++ iTrellisToDoList.Tests/TaskTests.cs
@@ -10,28 +10,28 @@ namespace iTrellisToDoList.Tests
         [TestMethod]
         public void Can_Set_Title_Property()
         {
-            Task testTask = new Task("Write novel", "Vampires are popular", new DateTime(2020, 01, 01));
+            Task testTask = LoadSampleTask();
             Assert.AreEqual(testTask.Title, "Write novel");
         }
 
         [TestMethod]
         public void Can_Set_Description_Property()
         {
-            Task testTask = new Task("Write novel", "Vampires are popular", new DateTime(2020, 01, 01));
+            Task testTask = LoadSampleTask();
             Assert.AreEqual(testTask.Description, "Vampires are popular");
         }
 
         [TestMethod]
         public void Can_Set_DueDate_Property()
         {
-            Task testTask = new Task("Write novel", "Vampires are popular", new DateTime(2020, 01, 01));
+            Task testTask = LoadSampleTask();
             Assert.AreEqual(testTask.DueDate, new DateTime(2020, 01, 01));
         }
-         
+
         [TestMethod]
         public void Can_Set_IsCompleted_Property()
         {
-            Task testTask = new Task("Write novel", "Vampires are popular", new DateTime(2020, 01, 01));
+            Task testTask = LoadSampleTask();
             testTask.IsCompleted = true;
             Assert.AreEqual(testTask.IsCompleted, true);
         }
@@ -39,8 +39,14 @@ namespace iTrellisToDoList.Tests
         [TestMethod]
         public void IsCompleted_Defaults_To_False()
         {
-            Task testTask = new Task("Write novel", "Vampires are popular", new DateTime(2020, 01, 01));
+            Task testTask = LoadSampleTask();
             Assert.AreEqual(testTask.IsCompleted, false);
+        }
+
+        public Task LoadSampleTask()
+        {
+            Task sampleTask = new Task("Write novel", "Vampires are popular", new DateTime(2020, 01, 01));
+            return sampleTask;
         }
     }
 }
